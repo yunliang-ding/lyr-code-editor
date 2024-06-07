3df7f34096b3af371554a563bafc434184d9031c
yunliang <804408086@qq.com>
2024-06-07 10:55:18 +0800

    remove eval5

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -28,7 +28,6 @@ export default defineConfig({
         react: 'React',
         'react-dom': 'ReactDOM',
         'react/jsx-runtime': 'jsxRuntime',
-        eval5: 'eval5',
       },
     },
   ],
